4c6fdbe8471bc1f6923b72adc1656f76c08f534e
Ahn Jeongmin <symbol9550@gmail.com>
2023-06-16 10:08:48 +0900

    비밀번호 비교기능 추가

--- src/main/java/com/sparta/guestbook/controller/GuestbookController.java
+++ src/main/java/com/sparta/guestbook/controller/GuestbookController.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping("/guestbook")
+@RequestMapping("/api")
 public class GuestbookController {
     private final Map<Long, Guestbook> guestbookList = new HashMap<>();
 
-    @PostMapping("/contents")
-    public GuestbookResponseDto creatContent(@RequestBody GuestbookRequestDto requestDto) {
+    @PostMapping("/post")
+    public GuestbookResponseDto creatPost(@RequestBody GuestbookRequestDto requestDto) {
         // RequestDtd -> Entity
         Guestbook guestbook = new Guestbook(requestDto);
 
@@ -30,35 +30,50 @@ public class GuestbookController {
         return responseDto;
     }
 
-    @GetMapping("/contents")
-    public List<GuestbookResponseDto> getContents() {
+    @GetMapping("/posts")
+    public List<GuestbookResponseDto> getPosts() {
         // Map To List
         List<GuestbookResponseDto> responseDtoList = guestbookList.values().stream()
                 .map(GuestbookResponseDto::new).toList();
         return responseDtoList;
     }
 
-    @PutMapping("/contents/{id}")
-    public Long updateContents(@PathVariable Long id, @RequestBody GuestbookRequestDto requestDto) {
+    @GetMapping("/post/{id}")
+    public GuestbookResponseDto getPost(@PathVariable Long id, @RequestBody GuestbookRequestDto requestDto){
+        Guestbook guestbook = guestbookList.get(id);
+        GuestbookResponseDto responseDto = new GuestbookResponseDto(guestbook);
+        return responseDto;
+    }
+
+    @PutMapping("/post/{id}")
+    public GuestbookRequestDto updateContents(@PathVariable Long id, @RequestBody GuestbookRequestDto requestDto) {
         // 해당 게시글이 데이터베이스에 존재하는지 확인
         if (guestbookList.containsKey(id)) {
             // 해당 게시글 가져오기
             Guestbook guestbook = guestbookList.get(id);
-
-            // 해당 게시글 수정
-            guestbook.update(requestDto);
-            return guestbook.getId();
+            if(requestDto.getPassword().equals(guestbookList.get(id).getPassword())) {
+                // 해당 게시글 수정
+                guestbook.update(requestDto);
+                return requestDto;
+            } else {
+                throw new IllegalArgumentException("비밀번호가 틀렸습니다.");
+            }
         } else {
             throw new IllegalArgumentException("선택한 게시글은 존재하지 않습니다.");
         }
     }
 
-    @DeleteMapping("/contents/{id}")
-    public Long deleteContents(@PathVariable Long id) {
+    @DeleteMapping("/post/{id}")
+    public String deleteContents(@PathVariable Long id, @RequestBody GuestbookRequestDto requestDto) {
         // 해당 게시글이 데이터베이스에 존재하는지 확인
         if (guestbookList.containsKey(id)) {
-            guestbookList.remove(id);
-            return id;
+            if(requestDto.getPassword().equals(guestbookList.get(id).getPassword())) {
+                // 해당 게시글 수정
+                guestbookList.remove(id);
+                return "success : true";
+            } else {
+                throw new IllegalArgumentException("비밀번호가 틀렸습니다.");
+            }
         } else {
             throw new IllegalArgumentException("선택한 게시글은 존재하지 않습니다.");
         }
